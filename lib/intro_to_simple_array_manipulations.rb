42610a0d341f1213b9fdf560fb48d2ab236002f7
Tara Ellicott <taraellicott@gmail.com>
2018-04-13 22:28:27 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,40 +1,40 @@
 def using_push(arr, str)
-arr.push(str)
+  arr.push(str)
 end
 
 
 def using_unshift(arr, str)
-arr.unshift(str)
+  arr.unshift(str)
 end
 
 
 def using_pop(arr)
-arr.pop
+  arr.pop
 end
 
 
 def pop_with_args(arr)
-arr.pop(2)
+  arr.pop(2)
 end
  
 
 def using_shift(arr)
-arr.shift
+  arr.shift
 end
   
 
 def shift_with_args(arr)
-arr.shift(2)
+  arr.shift(2)
 end
   
 
 def using_concat(arr1, arr2)
-arr1.concat(arr2)
+  arr1.concat(arr2)
 end
  
 
 def using_insert(arr, elem)
-arr.insert(4, elem)
+  arr.insert(4, elem)
 end
 
 
